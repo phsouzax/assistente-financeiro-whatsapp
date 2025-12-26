289bba88354b1e3d669aa5bc3963f5d7b95fc000
DevPedroSouza <dev.pedrosouzza3@gmail.com>
2025-12-26 01:30:59 -0300

    feat: deploy 24/7

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python whatsapp_financas.py
--- whatsapp_financas (3).py
+++ whatsapp_financas (3).py
@@ -749,4 +749,6 @@ def teste():
     return {'status': 'ok', 'mensagem': 'Envie POST com {"mensagem": "seu comando"}'}
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
